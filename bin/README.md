c02673c992b140ea2bb6a3f0029df96bcfa7598f
k26duran <k26duran@gmail.com>
2020-01-21 15:18:26 -0500

    operaciones con vectores, faltan pruebas

---
+++ bin/README.md
@@ -0,0 +1,4 @@
+﻿# Calculadora de números complejos
+
+En este laboratorio se desarrolla una calculadora para números complejos, que contiene las siguientes funcionalidades: suma, resta, multiplicación, división, módulo, fase, conjugada, y representación en coordenadas polares.
+Dentro del directorio se encontrara una carpeta /Test, en donde se encuentran diferentes pruebas para cada una de estas operaciones.
--- src/main/java/edu/escuelaing/cnyt/MathComplexNumber/MathComplex.java
+++ src/main/java/edu/escuelaing/cnyt/MathComplexNumber/MathComplex.java
@@ -97,6 +97,13 @@ public class MathComplex {
     public static ComplexNumber polarACartesiano(ComplexNumber c) {
         return new ComplexNumber(c.getReal() * Math.cos(c.getImaginario()), c.getReal() * Math.sin(c.getImaginario()));
     }
+    
+    /**
+     * Este método compara dos números complejos para saber si son iguales o no
+     * @param tupla1 número complejo a comparar
+     * @param tupla2 número complejo a comparar
+     * @return un boolean que indica si son iguales o no son iguales
+     */
 
     public static boolean igualdadComplejos(ComplexNumber tupla1, ComplexNumber tupla2) {
         double valorA1 = tupla1.getReal();
@@ -105,7 +112,148 @@ public class MathComplex {
         double valorB2 = tupla2.getImaginario();
         return valorA2 == valorA1 && valorB1 == valorB2;
     }
-
+    
+    /**
+     * Este método realiza la suma entre dos vectores dados y devuelve un nuevo
+     * vector de complejos. Para sumar dos vectores, se suma cada componente de un
+     * vector con el respectivo componente del otro vector.
+     *
+     * @param v1 vector de complejos.
+     * @param v2 vector de complejos.
+     * @return ComplexNumber[] : el vector de complejos resultante de la suma.
+     * @throws MathComplexException : si la longitud de los vectores es diferente,
+     *                              la suma vectorial no esta definida en los
+     *                              números complejos.
+     */
+    public static ComplexNumber[] sumarVectores(ComplexNumber[] v1, ComplexNumber[] v2) throws MathComplexException {
+        if (v1.length != v2.length) {
+            throw new MathComplexException(MathComplexException.LONGITUD_VECTORES_DIFERENTE);
+        }
+        ComplexNumber[] sumaVectores = new ComplexNumber[v1.length];
+        for (int i = 0; i < v1.length; i++) {
+            sumaVectores[i] = add(v1[i], v2[i]);
+        }
+        return sumaVectores;
+    }
+    
+    /**
+     * Este método calcula el inverso aditivo de un vector dado, el inverso aditivo de un vector
+     * es el inverso aditivo de cada uno de sus componentes, los cuales son un número complejo
+     * @param v vector de complejos
+     * @return ComplexNumber[] : el vector inverso de complejos
+     *  
+     */
+    public static ComplexNumber[] inversoVector(ComplexNumber[] v) {
+        ComplexNumber[] inversoVector = new ComplexNumber[v.length];
+        for (int i = 0; i < v.length; i++) {
+            inversoVector[i] = v[i].inversoAditivo();
+        }
+        return inversoVector;
+    }
+    
+    /**
+     * Este método calcula el conjugado de un vector de complejos. El conjugado de un vector de complejos es 
+     * el conjugado de cada uno de sus componentes que son números complejos 
+     * @param v vector de complejos
+     * @return ComplexNumber[] : el conjugado del vector de complejos
+     */
+    public static ComplexNumber[] conjugadoVector(ComplexNumber[] v) {
+        ComplexNumber[] conjugadoVector = new ComplexNumber[v.length];
+        for (int i = 0; i < v.length; i++) {
+            conjugadoVector[i] = v[i].Conjugado();
+        }
+        return conjugadoVector;
+    }
+    
+    /**
+     * Este método calcula la multiplicación entre un vector de números complejos y un número complejo.
+     * La multiplicación escalar se hace multiplicando el número complejo con cada uno de los 
+     * elementos del vector de números complejos.
+     * @param c número complejo
+     * @param v vector de complejos
+     * @return ComplexNumber[] : el vector resultado de la multiplicación escalar
+     */
+    public static ComplexNumber[] multiplicacionEscalarConVectores(ComplexNumber c, ComplexNumber[] v) {
+        ComplexNumber[] multiplicacionEscalarVectores = new ComplexNumber[v.length];
+        for (int i = 0; i < v.length; i++) {
+            multiplicacionEscalarVectores[i] = mult(c, v[i]);
+        }
+        return multiplicacionEscalarVectores;
+    }
+    
+    /**
+     * Este método calcula el producto interno entre dos vectores, también conocido
+     * como producto punto. Esta operación se calcula obteniendo el conjugado del
+     * primer vector, multiplicarlo por el segundo vector y por último sumar todos 
+     * los elementos del vector resultante.
+     * @param v1 vector de números complejos
+     * @param v2 vector de números complejos
+     * @return ComplexNumber[] : vector resultante del producto interno entre 
+     * dos vectores
+     * @throws MathComplexException si la longitud de los vectores es diferente,
+     *                              el producto interno no esta definido para los
+     *                              numeros complejos.
+     */
+    public static ComplexNumber productoInternoVectores(ComplexNumber[] v1, ComplexNumber[] v2)
+            throws MathComplexException {
+        if (v1.length != v2.length) {
+            throw new MathComplexException(MathComplexException.LONGITUD_VECTORES_DIFERENTE);
+        }
+        ComplexNumber productoInternoVector = new ComplexNumber();
+        ComplexNumber[] vC = conjugadoVector(v1);
+        for (int i = 0; i < v1.length; i++) {
+            productoInternoVector = add(productoInternoVector, mult(vC[i], v2[i]));
+        }
+        return productoInternoVector;
+    }
+    
+    /**
+     * Este método calcula la distancia entre dos vectores de números complejos.
+     * La distancia se halla de la siguiente manera: d = (productoInterno(v1-v2, v1-v2))^(1/2)
+     * @param v1 vector de números complejos
+     * @param v2 vector de números complejos
+     * @return Un double que es el resultado de la distancia entre dos vectores de números complejos
+     * @throws MathComplexException si la longitud de los vectores es diferente, entonces no se puede hallar 
+     * 								la distancia entre estos.
+     */
+    public static double distanciaEntreVectores(ComplexNumber[] v1, ComplexNumber[] v2) throws MathComplexException {
+        ComplexNumber[] resta = sumarVectores(v1, inversoVector(v2));
+        return Math.sqrt(productoInternoVectores(resta, resta).getReal());
+    }
+    
+    /**
+    * Este método calcula la norma o longitud de un vector de complejos.
+    * @param v vector complejo
+    * @return double : un numero real que representa la norma o longitud del vector v de complejos
+    * @throws MathComplexException : si la longitud de los vectores es diferente,
+    *                              el producto interno no esta definido para los
+    *                              numeros complejos.
+    */
+   public static double normaVector(ComplexNumber[] v) throws MathComplexException {
+       return Math.sqrt(productoInternoVectores(v, v).getReal());
+   }
+   
+   /**
+    * Este método compara dos vectores complejos. Para esto, verifica que cada uno
+    * de los componentes del primer vector sean iguales a los del segundo vector.
+    *
+    * @param v1 vector de complejos.
+    * @param v2 vector de complejos.
+    * @return boolean : true si son iguales, false si no lo son.
+    */
+   public static boolean equalsVector(ComplexNumber[] v1, ComplexNumber[] v2) {
+       boolean equals = true;
+       if (v1.length != v2.length) {
+           equals = false;
+       } else {
+           for (int i = 0; i < v1.length && equals; i++) {
+               if (!v1[i].equals(v2[i])) {
+                   equals = false;
+               }
+           }
+       }
+       return equals;
+   }
 
     
 
